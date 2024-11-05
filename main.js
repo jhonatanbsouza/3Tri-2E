c26b9e2f95d9f2a27955731b0f948ce05010c883
jhonatanbsouza <135228782+jhonatanbsouza@users.noreply.github.com>
2024-11-05 08:19:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function criaCartao(categoria,pergunta,resposta){
+    let container = document.getElementById('container')
+    let article = document.createElement('article')
+    article.className = 'cartao'
+    article.innerHTML = ` 
+        <div class="cartao-conteudo">
+            <h3> ${categoria} </h3>
+            <div class="cartao-conteudo-pergunta">
+                <p> ${pergunta} </p>
+            </div>
+            <div class="cartao-conteudo-resposta">
+                <p> ${resposta} </p>
+            </div>
+        </div>
+    `
+    container.appendChild(article)
+}
+
+criaCartao('Biologia','Qual a função da mitocondria?','Produção de Energia (ATP)')
+criaCartao('Matematica','Como é definido um grupo?','É um conjunto munido de uma operação onde esta é fechada no conjunto e tem algumas propriedades')
